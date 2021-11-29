589a5eaaefb7cc49faf39ab48e277f323af55bcc
Aaron <fong_lun@live.com>
2021-11-28 22:17:31 -0500

    ProductCollection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import styles from './App.module.css';
-import { Row, Col } from 'antd'
-import { Header, Footer, SideMenu, Carousel } from './components'
+import { Row, Col, Typography } from 'antd'
+import { Header, Footer, SideMenu, Carousel, ProductCollection } from './components'
+import { productList1, productList2, productList3 } from './mockups'
+import sideImage from './assets/images/sider_2019_12-09.png';
+import sideImage2 from './assets/images/sider_2019_02-04.png';
+import sideImage3 from './assets/images/sider_2019_02-04-2.png';
 
 function App() {
   return (
@@ -16,6 +20,21 @@ function App() {
             <Carousel />
           </Col>
         </Row>
+        <ProductCollection
+          products={productList1}
+          sideImage={sideImage}
+          title={<Typography.Title level={3} type='warning'>爆款推荐</Typography.Title>}
+        ></ProductCollection>
+        <ProductCollection
+          products={productList2}
+          sideImage={sideImage2}
+          title={<Typography.Title level={3} type='danger'>新品上市</Typography.Title>}
+        ></ProductCollection>
+        <ProductCollection
+          products={productList3}
+          sideImage={sideImage3}
+          title={<Typography.Title level={3} type='success'>国内游推荐</Typography.Title>}
+        ></ProductCollection>
       </div>
       <Footer />
     </div>
